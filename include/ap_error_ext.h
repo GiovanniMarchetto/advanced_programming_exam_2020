d53b6d706f6f6a393547a1fb8c60715ee0232ee7
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2021-02-09 19:17:01 +0100

    FIX: now the macro AP_ERROR_IF_NULLPTR works correctly with nullptr.

--- include/ap_error_ext.h
+++ include/ap_error_ext.h
@@ -14,32 +14,38 @@
 #ifndef ADVANCED_PROGRAMMING_EXAM_2020_AP_ERROR_H
 #define ADVANCED_PROGRAMMING_EXAM_2020_AP_ERROR_H
 
+ /** Returns true it the template type is a pointer.*/
+template<typename T>
+inline bool is_pointer(const T&)
+{
+    return std::is_pointer<T>::value;
+}
+
+
+/**
+ * \def AP_ERROR_ARGS(var_type, var)
+ * This macro supports the macro AP_ASSERT_IF_NULLPTR, providing arguments for
+ * the macro AP_ERROR(...) and AP_ASSERT(...).
+ */
+#define AP_ERROR_ARGS(var)                                                    \
+    (!(is_pointer(var) && ! var )), std::invalid_argument /*check if it is not nullptr*/
 
  /**
-  * \def     AP_ERROR_ARGS(var_type, var)
-  * This macro supports the macro AP_ASSERT_IF_NULLPTR, providing arguments for
-  * the macro AP_ERROR(...) and AP_ASSERT(...).
+  * \def AP_ASSERT_IF_NULLPTR(var_type, var, msg, is_error )
+  * This macro checks if a variable is a nullptr, by using AP_ERROR or AP_ASSERT.
+  * @param var      The variable.
+  * @param var_name The name of the variable.
+  * @param is_error Flag: if true, AP_ERROR is used, otherwise AP_ASSERT.
   */
-#define AP_ERROR_ARGS(var_type, var)                                          \
-    (!(std::is_pointer< var_type >::value && ! var )), std::invalid_argument
-
-  /**
-   * \def AP_ASSERT_IF_NULLPTR(var_type, var, msg, is_error )
-   * This macro checks if a variable is a nullptr, by using AP_ERROR or AP_ASSERT.
-   * @param var_type The type of the variable.
-   * @param var      The variable.
-   * @param var_name The name of the variable.
-   * @param is_error Flag: if true, AP_ERROR is used, otherwise AP_ASSERT.
-   */
-#define AP_ERROR_IF_NULLPTR(var_type, var, var_name, is_error )               \
+#define AP_ERROR_IF_NULLPTR(var, var_name, is_error )                         \
     std::string msg {"The variable " #var_name " cannot be nullptr."};        \
     if(is_error)                                                              \
     {                                                                         \
-        AP_ERROR(AP_ERROR_ARGS(var_type, var)) << msg << std::endl;           \
+        AP_ERROR(AP_ERROR_ARGS(var)) << msg << std::endl;                     \
     }                                                                         \
     else                                                                      \
     {                                                                         \
-        AP_ASSERT(AP_ERROR_ARGS(var_type, var)) << msg << std::endl;          \
+        AP_ASSERT(AP_ERROR_ARGS(var)) << msg << std::endl;                    \
     }
 
 
